23e8781b04748cd9c84da295c14bf75755274c0b
Jeferson Coutinho <jefissuu@gmail.com>
2025-03-01 09:24:01 -0300

    test(SearchAccountsUseCase): add test coverage

--- composeApp/src/commonTest/kotlin/com/jefisu/authenticator/data/FakeAccountRepository.kt
+++ composeApp/src/commonTest/kotlin/com/jefisu/authenticator/data/FakeAccountRepository.kt
@@ -5,27 +5,35 @@ import com.jefisu.authenticator.domain.repository.AccountRepository
 import com.jefisu.authenticator.domain.util.Error
 import com.jefisu.authenticator.domain.util.Result
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class FakeAccountRepository : AccountRepository {
 
-    private val accounts = mutableListOf<TwoFactorAuthAccount>()
     private var shouldFail = false
+    private val accountFlow = MutableStateFlow(emptyList<TwoFactorAuthAccount>())
+    private var accounts = emptyList<TwoFactorAuthAccount>()
+        set(value) {
+            field = value
+            accountFlow.update { value }
+        }
 
     override suspend fun addAccount(account: TwoFactorAuthAccount): Result<Unit, Error> {
         checkFailure()?.let { return it }
 
-        return accounts.add(account).let { Result.Success(Unit) }
+        accounts += account
+        return Result.Success(Unit)
     }
 
     override suspend fun deleteAccount(account: TwoFactorAuthAccount): Result<Unit, Error> {
         checkFailure()?.let { return it }
 
-        return accounts.remove(account).let { Result.Success(Unit) }
+        accounts -= account
+        return Result.Success(Unit)
     }
 
     override fun getAllAccounts(): Flow<List<TwoFactorAuthAccount>> {
-        return flowOf(accounts)
+        return accountFlow
     }
 
     fun forceNextOperationFailure() {
